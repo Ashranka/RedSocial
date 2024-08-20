69caa5bc4defc703cf75ca4b387dfb8bc3f91719
Andres Shranka <andreshranka@gmail.com>
2024-08-19 23:23:17 -0400

    Add project files.

---
+++ RedSocial/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RedSocial.Data;
+using RedSocial.Entities;
+
+namespace RedSocial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _dataContext.AppUsers.ToListAsync();
+            
+            return Ok(users);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id )
+        {
+            var result = await _dataContext.AppUsers.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (result == null) return NotFound();
+
+            return Ok(result);
+        }
+
+    }
+}
---
+++ RedSocial/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedSocial.Entities;
+
+namespace RedSocial.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
